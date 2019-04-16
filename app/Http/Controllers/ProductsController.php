8f1057f31913a9dc7cef758faa6f274410e8bbd5
Happylim99 <happylim99@gmail.com>
2019-04-16 22:33:25 +0800

    Product collection transformation

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Model\Product;
 use Illuminate\Http\Request;
 use App\Http\Resources\Product\ProductResource;
+use App\Http\Resources\Product\ProductCollection;
 
 class ProductsController extends Controller
 {
@@ -15,7 +16,9 @@ class ProductsController extends Controller
      */
     public function index()
     {
-        return Product::all();
+        return ProductCollection::collection(Product::all());
+        //return ProductResource::collection(Product::all());
+        //return Product::all();
     }
 
     /**
